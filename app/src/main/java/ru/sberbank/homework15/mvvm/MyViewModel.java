e2b2647728ec7633a4537db25634000b2934e1fb
Alexander <sapfirmoscow@yandex.ru>
2019-03-06 13:33:27 +0300

    Completed

--- app/src/main/java/ru/sberbank/homework15/mvvm/MyViewModel.java
+++ app/src/main/java/ru/sberbank/homework15/mvvm/MyViewModel.java
@@ -5,27 +5,19 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Looper;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import ru.sberbank.homework15.model.Picture;
 
 public class MyViewModel extends ViewModel {
 
-    private final ExecutorService mService;
-    private final Handler mHandler;
     private final MyModel mModel;
     private MutableLiveData<List<Picture>> mPictures;
 
     public MyViewModel() {
         super();
         mModel = new MyModel();
-        mService = Executors.newSingleThreadExecutor();
-        mHandler = new Handler(Looper.getMainLooper());
     }
 
     public LiveData<List<Picture>> getPictures() {
